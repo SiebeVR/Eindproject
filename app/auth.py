b9faa6fe02ac1f4b3c86892b8cd8d87cb580ad37
siebe <r0804405@student.thomasmore.be>
2023-01-04 20:06:22 +0100

    jee

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from crud import get_user_by_username
+from app.crud import get_user_by_username
 from sqlalchemy.orm import Session
 from passlib.context import CryptoContext
 from jose import JWTError, jwt
